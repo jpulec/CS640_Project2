3d215992951c172c5bd57736cb745acc62db9e0e
Brian Ploeckelman <ploeckelman@wisc.edu>
2012-11-15 03:13:58 -0600

    Added some scripts to simplify testing
    Log in to mumble-30 (or change scripts locally to point to whichever server)
    
    This cleans, builds everything, and starts the emulator
    $ ./deploy.sh
    
    In another terminal: (note: sender isn't updated for p2 yet)
    $ ./startSender.sh
    
    In another terminal:
    $ ./startRequester.sh

---
+++ startRequester.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+printf "Starting requester on port 4000\n";                            \
+printf "\temulator @ mumble-30:3000, file: file.txt, window: 100\n\n"; \
+./requester -p 4000 -f mumble-30 -h 3000 -o file.txt -w 100
+
---
+++ startSender.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+printf "Starting sender on port 2000\n";                   \
+printf "\trequester on port 4000, rate: 100, len: 100,\n"; \
+printf "\temulator @ mumble-30:3000, i: 2, t: 100\n\n";    \
+./sender -p 2000 -g 4000 -r 100 -q 1 -l 100 -f mumble-30 -h 3000 -i 2 -t 100
